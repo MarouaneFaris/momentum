1882313b74c0d7a5580c0e182404f2ce02aa2ba1
Marouane Faris <marouanefaris@gmail.com>
2026-05-10 17:32:26 +0200

    feat(frontend): config ts config + linters

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicssl from '@vitejs/plugin-basic-ssl'
+import basicssl from "@vitejs/plugin-basic-ssl";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    basicssl(),
-  ],
-  server: {
-    host: true,
-    port: 3000,
-    https: true,
-  },
-})
+    plugins: [react(), basicssl()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        host: true,
+        port: 3000,
+    },
+});
