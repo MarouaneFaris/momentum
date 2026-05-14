43f923bc367488a96a7883b87857e695f56f5da6
Marouane Faris <marouanefaris@gmail.com>
2026-05-14 19:53:50 +0200

    fix(user): php-stan errors

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -17,7 +17,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
-    private ?string $email = null;
+    private string $email;
 
     /**
      * @var list<string> The user roles
@@ -29,7 +29,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
-    private ?string $password = null;
+    private string $password;
 
     public function getId(): ?int
     {
