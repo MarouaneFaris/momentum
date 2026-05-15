47f5a94efb0d490bbc1bc78233701aa6de756e57
Marouane Faris <marouanefaris@gmail.com>
2026-05-15 20:52:23 +0200

    feat(readme): test failed badge

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -17,7 +17,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
-    private string $email;
+    private ?string $email = null;
 
     /**
      * @var list<string> The user roles
