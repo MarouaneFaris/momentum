63a1a0f1c1a94a3c35d3f7fc26b47bf29539cf0a
Marouane Faris <marouanefaris@gmail.com>
2026-05-17 18:09:41 +0200

    feat(react-query): add react-query debugger

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,14 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 
 const queryClient = new QueryClient()
 
+declare global {
+    interface Window {
+        __TANSTACK_QUERY_CLIENT__: import('@tanstack/query-core').QueryClient
+    }
+}
+
+window.__TANSTACK_QUERY_CLIENT__ = queryClient
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
