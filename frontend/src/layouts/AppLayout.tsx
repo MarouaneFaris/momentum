1ccde700ee294fc6369e0c071cbc22ec352d7956
Marouane Faris <marouanefaris@gmail.com>
2026-05-22 20:23:53 +0200

    feat(theme-toggle): handle ligh/dark/system theme instruction (#30)
    * feat(darkmode): add dropdown menu component
    
    * feat(theme-toggle): handle ligh/dark/system theme instruction

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,24 +1,13 @@
+import { ThemeToggle } from '@/components/ThemeToggle'
 import { Button } from '@/components/ui/button'
 import { AuthContext } from '@/contexts/auth/AuthContext'
 import { useLogoutAction } from '@/features/auth/hooks/useLogoutAction'
-import { Moon, Sun } from 'lucide-react'
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { Navigate, Outlet } from 'react-router'
 
 export default function AppLayout() {
     const auth = useContext(AuthContext)
-    const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('isDarkMode') === 'enabled')
     const { handleOnLogout } = useLogoutAction()
-
-    useEffect(() => {
-        if (isDarkMode) {
-            document.querySelector('body')?.classList.add('dark')
-        } else {
-            document.querySelector('body')?.classList.remove('dark')
-        }
-
-        localStorage.setItem('isDarkMode', isDarkMode ? 'enabled' : 'disabled')
-    }, [isDarkMode])
 
     if (auth.isLoading) {
         return 'Loading...'
@@ -30,14 +19,7 @@ export default function AppLayout() {
 
     return (
         <>
-            <Button
-                type="button"
-                size="icon"
-                variant="outline"
-                onClick={() => setIsDarkMode(!isDarkMode)}
-            >
-                {isDarkMode ? <Sun /> : <Moon />}
-            </Button>
+            <ThemeToggle />
             <Button type="button" onClick={handleOnLogout}>
                 Logout
             </Button>
