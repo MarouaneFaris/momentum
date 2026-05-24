2b38e20ea52e95aff828f55c2ea28346e9c6d238
Marouane Faris <marouanefaris@gmail.com>
2026-05-24 21:24:07 +0200

    chore(tests): install symfony package for tests

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
